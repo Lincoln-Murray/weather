c5bb038dbfe68d0c75372309d0ea65b36b247033
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-08-12 23:11:34 +0800

    Change api call

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ location = ''
 @app.route("/")
 def index():
     key = 'ad261dda6b984968bd560251243107'
-    url = 'http://api.weatherapi.com/v1/forecast.json?key=' + key + '&q=' + location + '&aqi=yes'
+    url = 'http://api.weatherapi.com/v1/forecast.json?key=' + key + '&q=' + location + '&days=3&aqi=yes&alerts=yes'
 
     response = r.get(url)
 
